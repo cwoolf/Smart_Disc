21884352dd38952f18c236c6bae8cd0d2b3b9d41
salvadorhol <salvadorhol@gmail.com>
2014-04-13 06:21:24 -0400

    Final
    Functional version, tracks altitude and speed.

--- beta/beta.ino
+++ beta/beta.ino
@@ -1,171 +1,186 @@
- #include "TinyGPS.h"
+//Includes
 #include <SoftwareSerial.h>
+#include <TinyGPS++.h>
 
-//gps serial port RX, TX
-SoftwareSerial gss(9, 10);
-SoftwareSerial device(3, 11);
+//PINS SETTINGS
+const int buttonPin = 2;    // the number of the pushbutton pin
+const int ledPin = 13;      // the number of the LED pin
 
-int incomingByte = 0;
+// Variables will change:
+int ledState = LOW;         // the current state of the output pin
+int ledLast = LOW;
+int buttonState;             // the current reading from the input pin
+int lastButtonState = LOW;   // the previous reading from the input pin
 
-//keep track of how long push button was pressed
-int state = HIGH;
-int reading;
-int previous = LOW;
-long time = 0;
-int debounce = 200; //the debounce time, increase if the output flickers
+// the following variables are long's because the time, measured in miliseconds,
+// will quickly become a bigger number than can be stored in an int.
+long lastDebounceTime = 0;  // the last time the output pin was toggled
+long debounceDelay = 50;    // the debounce time; increase if the output flickers
 
-boolean runGPS = false;
+//Serial ports RX, TX
+SoftwareSerial gss(10, 11);
+SoftwareSerial lss(3, 9);
+static const uint32_t Baud = 9600;
+int fileNumber;
+char buff[50];
 
-TinyGPS gps;
+TinyGPSPlus gps;
 
-void setUpdateRate()
+void file()
 {
-  device.write((byte)0xA0); //A0
-  //device.print(160, HEX);
-  delay(200);
-  
-  device.write((byte)0xA1);  //A1
-  //device.print(161, BIN);
-  delay(200);
-  
-  device.write((byte)0x00);    //00
-  //device.print(0, BIN);
-  delay(200);
-  
-  device.write((byte)0x03);    //03
-  //device.print(3, BIN);
-  delay(200);
-  
-  device.write((byte)0x0E);   //0E
-  //device.print(14, BIN);
-  delay(200);
-  
-  device.write((byte)0x05);   //10 10Hz
-  //device.print(05, BIN);
-  delay(200);
-  
-  device.write((byte)0x01);
-  //device.print(1, HEX);
-  delay(200);
-  
-  device.write((byte)0x0A);
-  //device.print(10, HEX);
-  delay(200);
-  
-  device.write((byte)0x0D);
-  //device.print(13, BIN);
-  delay(200);
-
-  device.write((byte)0x0A);
-  //device.print(10, HEX);
-  delay(200);
+ gss.end();
+ delay(100);
+ lss.begin(Baud);
  
-  if (gss.available() > 0)
-  {
-    Serial.print(gss.read(), HEX);
-  }
+ //Reset OpenLog
+  digitalWrite(A3, LOW);
+  delay(100);
+  digitalWrite(A3, HIGH);
+
+  //Works with Arduino v1.0
+  lss.write(26);
+  lss.write(26);
+  lss.write(26);
+  
+  lss.print("new file.txt\r");
+  //Wait for OpenLog to return to waiting for a command
+ 
+  //sprintf(buff, "append nate%03d.txt\r", fileNumber);
+  lss.print("append sallll.txt\r");
+  
+  lss.print("ready: ");
+  
+  lss.end();
+  delay(100);
+  gss.begin(Baud);
 }
 
-void setBaud()
-{
-  device.write((byte)0xA0); //A0
-  
-  device.write((byte)0xA1);  //A1
-  
-  device.write((byte)0x00);    //00
-  
-  device.write((byte)0x04);    //03
-  
-  device.write((byte)0x05);   //0E
-  
-  device.write((byte)0x00);   //10 10Hz
-  
-  device.write((byte)0x01);
-  
-  device.write((byte)0x00);
-  
-  device.write((byte)0x04);
-  
-  device.write((byte)0xD0);
 
-  device.write((byte)0xA0);
+void setup() {
+  
+  gss.begin(Baud);
+  
+  pinMode(buttonPin, INPUT);
+  pinMode(ledPin, OUTPUT);
+  pinMode(A4, OUTPUT);
+  pinMode(A3, OUTPUT);
+  
+  randomSeed(analogRead(0));
  
-  if (gss.available() > 0)
-  {
-    Serial.print(gss.read(), HEX);
-  }
+  // set initial LED state
+  digitalWrite(ledPin, ledState);
+  
+  //file();
 }
 
-void setup()
+void newCourse()
 {
-  pinMode(2, INPUT);
-  pinMode(3, OUTPUT);
-  pinMode(11, OUTPUT);
+  gss.end();
+  lss.begin(9600);
   
-  Serial.begin(9600);
+  if (ledLast == LOW)
+  {
+    lss.println("New Course ==========================================");
+  }
+  else if (ledLast == HIGH)
+  {
+    lss.println("END =================================================");
+    lss.println("");
+    lss.println("");
+  }
+  
+  lss.end();
   gss.begin(9600);
-  
-  attachInterrupt(2, pushButton, CHANGE);
 }
 
 void pushButton()
 {
-  reading = digitalRead(2);
+  // read the state of the switch into a local variable:
+  int reading = digitalRead(buttonPin);
+
+  // check to see if you just pressed the button 
+  // (i.e. the input went from LOW to HIGH),  and you've waited 
+  // long enough since the last press to ignore any noise:  
+
+  // If the switch changed, due to noise or pressing:
+  if (reading != lastButtonState) {
+    // reset the debouncing timer
+    lastDebounceTime = millis();
+  } 
   
-  if (reading == HIGH && previous == LOW && millis() - time > debounce)
-  {
-    if (state == HIGH)
-    {
-      state = LOW;
-      digitalWrite(13, LOW);
-      runGPS = false;
-    }
-    else
-    {
-      state = HIGH;
-      digitalWrite(13, HIGH);
-      runGPS = true;
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    // whatever the reading is at, it's been there for longer
+    // than the debounce delay, so take it as the actual current state:
+
+    // if the button state has changed:
+    if (reading != buttonState) {
+      buttonState = reading;
+
+      // only toggle the LED if the new button state is HIGH
+      if (buttonState == HIGH) {
+        ledLast = ledState;
+        ledState = !ledState;
+        newCourse();
+      }
     }
   }
   
-  time = millis();
+  // set the LED:
+  digitalWrite(ledPin, ledState);
+  digitalWrite(A4, ledState);
+
+  // save the reading.  Next time through the loop,
+  // it'll be the lastButtonState:
+  lastButtonState = reading;
 }
 
-void getLocation()
-{
-  int c = gss.read();
+void loop() {
   
-  if (gps.encode(c))
+  pushButton();
+  
+   while (gss.available() > 0 && ledState == HIGH)
+   {
+    if (gps.encode(gss.read()))
+    {
+      gss.end();
+      lss.begin(Baud);
+        displayInfo();
+      lss.end();
+      gss.begin(Baud);   
+    }
+   }  
+}
+
+void displayInfo()
+{
+  if (gps.location.isValid())
   {
     
-    long lat, lon;
-    unsigned long fix_age, speeds;
-    
-    gps.get_position(&lat, &lon, &fix_age);
-    speeds = gps.speed();
+    lss.print(gps.location.lat(), 6);
+    lss.print(F(", "));
+    lss.print(gps.location.lng(), 6);
+    lss.print(F(", "));
+    lss.print(gps.altitude.meters());
+    lss.print(F(", "));
+    lss.print(gps.speed.mps());
+    lss.print(F(", "));
+    lss.print(gps.speed.mph());
+    lss.print(F(", "));
+    lss.print(gps.time.hour()-4);
+    lss.print(F(":"));
+    if (gps.time.minute() < 10) lss.print(F("0"));
+    lss.print(gps.time.minute());
+    lss.print(F(":"));
+    if (gps.time.second() < 10) lss.print(F("0"));
+    lss.print(gps.time.second());
    
-    Serial.print("Lat: ");
-    Serial.print( lat );
-    gss.print("Lat: ");
-    gss.print( lat );
-    
-    Serial.print(", Lon: ");
-    Serial.print( lon );
-    Serial.print(" -- speed: ");
-    Serial.println(speeds);
-    gss.print(", Lon: ");
-    gss.println( lon );
-    gss.print(" -- speed: ");
-    gss.println(speeds);
     
   }
-  
-}
+  else
+  {
+    Serial.print(F("INVALID"));
+  }
+ 
 
-void loop()
-{
-    while (gss.available())
-    {
-       getLocation();
-    }
+  lss.println();
 }
